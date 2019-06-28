2109e8b0378524187e32d5ed3504f277d2705507
ejdeposit <edeposit@pdx.edu>
2019-06-27 19:26:58 -0700

    fixed loops in render functions and passed test 2

--- asciilines.py
+++ asciilines.py
@@ -23,7 +23,6 @@ def print_canvas(canvas, row, col):
 
 def render_char(line, canvas):
     lineList= line.split(' ', 5)
-    #print('lineList: ', lineList)
 
     char=lineList[0]
     rowStart=int(lineList[1])
@@ -33,12 +32,12 @@ def render_char(line, canvas):
 
     if lineDir == 'h':
         if rowStart in canvas:
-            for i in range(colStart, length+1):
+            for i in range(colStart, length+colStart):
                 if i in canvas[rowStart]:
                     canvas[rowStart][i]= char
 
     elif lineDir == 'v':
-        for i in range(rowStart, length+1):
+        for i in range(rowStart, length+rowStart):
             if i in canvas and colStart in canvas[i]:
                 canvas[i][colStart]= char
     else:
@@ -58,12 +57,10 @@ for line in fin:
         line=line[:-1:]
 
     if lineCount == 0:
-        #print('canvas dimensions: ', line)
         rowCol= line.split(' ', 2)
         row=int(rowCol[0])
         col=int(rowCol[1])
         canvas= fill_canvas(row, col)
-        #print_canvas(canvas, row, col)
     else:
         render_char(line, canvas)
 
